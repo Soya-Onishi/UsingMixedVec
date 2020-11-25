e44c1ceaeb2680044898bf5713ae28a45ded63e1
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-11-25 10:25:21 +0900

    Fix to be more simple

--- src/main/scala/example/package.scala
+++ src/main/scala/example/package.scala
@@ -1,15 +1,7 @@
 import chisel3._
 
 package object example {
-  trait ToVec[T <: Data] {
-    def convertToVec(seq: Seq[T]): Vec[T]
-  }
-
-  implicit def fromSeqToVec[T <: Data]: ToVec[T] = new ToVec[T] {
-    override def convertToVec(seq: Seq[T]): Vec[T] = VecInit(seq)
-  }
-
   implicit class ToMixedVecConv[T <: Data : ToVec](seq: Seq[T]) {
-    def toVec: Vec[T] = implicitly[ToVec[T]].convertToVec(seq)
+    def toVec: Vec[T] = VecInit(seq)
   }
 }
