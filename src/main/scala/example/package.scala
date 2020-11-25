868e03c184e4ad11212aa4cdd88dfa543519bcaa
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-11-25 10:25:41 +0900

    Fix to be more simple

--- src/main/scala/example/package.scala
+++ src/main/scala/example/package.scala
@@ -1,7 +1,7 @@
 import chisel3._
 
 package object example {
-  implicit class ToMixedVecConv[T <: Data : ToVec](seq: Seq[T]) {
+  implicit class ToMixedVecConv[T <: Data](seq: Seq[T]) {
     def toVec: Vec[T] = VecInit(seq)
   }
 }
